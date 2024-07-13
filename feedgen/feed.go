b4eadb3a867a58f7a69d5eb4af3d293e7c0ca80f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-07-13 04:57:50 -0700

    bsky/feedgen.Feed

---
+++ feedgen/feed.go
@@ -0,0 +1,37 @@
+package feedgen
+
+import (
+	"encoding/json"
+)
+
+type Feed interface {
+	json.Marshaler
+	FeedLen() int
+	Items() []Item
+}
+
+type internalFeed struct {
+	items []Item
+}
+
+var _ json.Marshaler = internalFeed{}
+
+func FeedItems(items ...Item) Feed {
+	return internalFeed{
+		items:items,
+	}
+}
+
+func (receiver internalFeed) FeedLen() int {
+	return len(receiver.items)
+}
+
+func (receiver internalFeed) Items() []Item {
+	return receiver.items
+}
+
+func (receiver internalFeed) MarshalJSON() ([]byte, error) {
+	var data []Item = receiver.Items()
+
+	return json.Marshal(data)
+}

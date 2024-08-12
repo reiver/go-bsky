aa8f08bb2e4a028038a00c18ea64e331dc86acb6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-11 20:37:53 -0700

    bluesky firehose

---
+++ firehose/message.go
@@ -0,0 +1,69 @@
+package firehose
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/brianolson/cbor_go" // because it can handle extra data after the (first) CBOR
+	cbor2 "github.com/fxamacker/cbor/v2" // because it can decode into a map
+	"github.com/reiver/go-erorr"
+)
+
+// Message represents a message from the Bluesky Firehose websocket.
+type Message []byte
+
+// Decode decodes the message from the Bluesky Firehose websocket into the message-header and the message-payload.
+func (receiver Message) Decode(header *MessageHeader, payload *MessagePayload) error {
+
+	//@TODO: This is NOT an ideal way of decoding this.
+	//       Maybe implement a CBOR codec later to do this better.
+
+	if nil == header {
+		return errNilMessageHeader
+	}
+	if nil == payload {
+		return errNilPayload
+	}
+
+	{
+		var reader io.Reader = bytes.NewReader(receiver)
+		if nil == reader {
+			return errNilReader
+		}
+
+		var decoder *cbor.Decoder = cbor.NewDecoder(reader)
+		if nil == decoder {
+			return errNilDecoder
+		}
+
+		err := decoder.Decode(header)
+		if nil != err {
+			return erorr.Errorf("bsky: problem decoding cbor header: %w", err)
+		}
+	}
+
+	var headersize int
+	{
+		var builder strings.Builder
+
+		err := cbor.Encode(&builder, header)
+		if nil != err {
+			return erorr.Errorf("bsky: problem encoding cbor header (after decoding it, to get length): %w", err)
+		}
+
+		headersize = builder.Len()
+	}
+
+	{
+		var p []byte = receiver[headersize:]
+
+		err := cbor2.Unmarshal(p, &payload)
+		if nil != err {
+			return erorr.Errorf("bsky: problem decoding cbor payload: %w", err)
+		}
+	}
+
+	return nil
+}

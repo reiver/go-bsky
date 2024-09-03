131eaece15667bf3840060ca83155461ac52caff
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-09-03 12:40:23 -0700

    app.bsky.graph.follow

--- app/bsky/graph/follow.go
+++ app/bsky/graph/follow.go
@@ -3,6 +3,6 @@ package graph
 // Follow represents a 'app.bsky.graph.follow'.
 type Follow struct {
 	Type      string `json:"$type"`
-	Subject   string `json:"subject"`
 	CreatedAt string `json:"createdAt"`
+	Subject   string `json:"subject"`
 }
